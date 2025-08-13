ebc24a3e8589856c65d2d2bb564d082ab02498e9
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-08-13 16:51:28 -0300

    qrcode terminal instalado

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import prompt from "prompt"
 import mainPrompt from "./prompts/prompt.main.js";
+import chalk from "chalk";
 
 async function main() {
     prompt.get(mainPrompt, async function(err,choose){
         if(choose.select == 1){
-            console.log("Escolheu qrCODE")
+            console.log(chalk.green("Escolheu qrCODE"))
         }
 
         if(choose.select == 2){
-            console.log("Escolheu Password")
+            console.log(chalk.green("Escolheu Password"))
         }
     });
 
