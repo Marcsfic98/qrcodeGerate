8ee4f1a375e50306deee887d96efac993574bb4c
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-08-15 10:08:42 -0300

    funçao de criar senha criada

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import prompt from "prompt"
 import mainPrompt from "./prompts/prompt-main.js";
-import chalk from "chalk";
 import createQRCode from "./services/qrcode/create.js";
+import createPassword from "./services/password/create.js";
 
 async function main() {
     prompt.get(mainPrompt, async function(err,choose){
@@ -10,7 +10,7 @@ async function main() {
         }
 
         if(choose.select == 2){
-            console.log(chalk.green("Escolheu Password"))
+            await createPassword
         }
     });
 
---
+++ src/services/password/create.js
@@ -0,0 +1,9 @@
+import chalk from "chalk";
+
+
+
+async function createPassword() {
+    console.log(chalk.green("Password"))
+}
+
+export default createPassword
